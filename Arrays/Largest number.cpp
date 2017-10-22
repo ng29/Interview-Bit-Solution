8469d497db09242196b1a51d7ff15b3f3ad14317
Nitin Gupta <nitingupta2906@gmail.com>
2017-10-22 23:51:06 +0530

    Create Largest number.cpp

---
+++ Arrays/Largest number.cpp
@@ -0,0 +1,20 @@
+string sort(vector<int>A);
+int myComp(string x, string y){
+    return x+y > y+x;
+}
+
+string Solution::largestNumber(const vector<int> &A) {
+    int i;
+    vector<string>B;
+    string ans="";
+    bool all=true;
+    
+    for(i=0;i<A.size();i++){
+        B.push_back(to_string(A[i]));
+        if(A[i]!=0) all=false;
+    }
+    if(all) return "0";
+    sort(B.begin(),B.end(),myComp);
+    for(i=0;i<B.size();i++)     ans+=B[i];
+    return ans;
+}
