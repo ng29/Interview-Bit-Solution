230e4f8169142b687f45f9fce32caa981975d0e2
Nitin Gupta <nitingupta2906@gmail.com>
2017-10-22 23:50:22 +0530

    Create Wave Array.cpp

---
+++ Arrays/Wave Array.cpp
@@ -0,0 +1,12 @@
+vector<int> Solution::wave(vector<int> &A) {
+     int j,i,l,temp,a;
+     l=A.size();
+    sort(A.begin(),A.begin()+l);
+    
+     for(i=0;i<l-1;i+=2){
+         temp=A[i];
+         A[i]=A[i+1];
+         A[i+1]=temp;
+     }
+     return A;
+}
