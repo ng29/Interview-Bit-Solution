53e0c25830453e62a6caa073c512adca59f1dd3b
Nitin Gupta <nitingupta2906@gmail.com>
2017-10-23 16:40:27 +0530

    Create Hotel Bookings Possible.cpp

---
+++ Arrays/Hotel Bookings Possible.cpp
@@ -0,0 +1,20 @@
+bool Solution::hotel(vector<int> &arrive, vector<int> &depart, int K) {
+    int a=0,d=0,start,end,l;
+    l=arrive.size();
+    sort(arrive.begin(),arrive.end());
+    sort(depart.begin(),depart.end());
+    start=arrive[0];
+    end=depart[l-1];
+    for(;start<=end;start++){
+        while((d<l) && (start==depart[d])){
+            K++;
+            d++;
+        }
+        while((a<l) && (start==arrive[a])){
+            if(K>0) K--;
+            else    return false;
+            a++;
+        }
+}
+return true;
+}
