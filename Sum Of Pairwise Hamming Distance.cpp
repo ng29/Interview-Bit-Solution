4ff423caaf6b83c156a5fc3b8a63d859b913ae89
Nitin Gupta <nitingupta2906@gmail.com>
2017-10-25 23:42:29 +0530

    Create Sum Of Pairwise Hamming Distance.cpp

---
+++ Sum Of Pairwise Hamming Distance.cpp
@@ -0,0 +1,13 @@
+int Solution::hammingDistance(const vector<int> &A) {
+
+    int i,j;
+    long long int ans=0;
+    for(i=0;i<31;i++){
+        int c=0;
+        for(j=0;j<A.size();j++)
+            if(A[j]&(1<<i))
+            c++;
+        ans+=(c*(A.size()-c)*2);
+    }
+    return ans%1000000007;
+}
