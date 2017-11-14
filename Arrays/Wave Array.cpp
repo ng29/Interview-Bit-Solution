a32a923eec4a9c4940017718ab641755584ecc41
Nitin Gupta <nitingupta2906@gmail.com>
2017-11-15 00:06:08 +0530

    Update Wave Array.cpp

--- Arrays/Wave Array.cpp
+++ Arrays/Wave Array.cpp
@@ -1,6 +1,6 @@
 vector<int> Solution::wave(vector<int> &A) {
-     int j,i,l,temp,a;
-     l=A.size();
+     int j,i,temp,a;
+     int l=A.size();
     sort(A.begin(),A.begin()+l);
     
      for(i=0;i<l-1;i+=2){
