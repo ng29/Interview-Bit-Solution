ad99193c331746fcaa8d8e5499c7df7d4b788c8c
Nitin Gupta <nitingupta2906@gmail.com>
2018-01-07 11:21:16 +0530

    Create MergeTwoSortedList.cpp

---
+++ LinkedList/MergeTwoSortedList.cpp
@@ -0,0 +1,26 @@
+ListNode* Solution::mergeTwoLists(ListNode* A, ListNode* B) {
+    if(A==NULL) return B;
+    if(B==NULL) return A;
+    
+    ListNode* head=NULL;
+    if(A->val<B->val){
+        head=A;
+        A=A->next;
+    }else{
+        head=B;
+        B=B->next;
+    }
+    ListNode* p=head;
+    while(A && B){
+        if(A->val<B->val){
+        p->next=A;
+        A=A->next;
+    }else{
+        p->next=B;
+        B=B->next;
+    }p=p->next;
+    }
+    if(A)   p->next=A;
+    else    p->next=B;
+    return head;
+}
